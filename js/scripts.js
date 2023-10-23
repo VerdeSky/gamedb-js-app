978194397b2b7173c4f49ae7a78c60ac4ec241fa
VerdeSky <140350863+VerdeSky@users.noreply.github.com>
2023-10-23 14:18:12 -0700

    created array and added objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
-alert('Hello World!')
-let favFood = 'French Fries';
-document.write(favFood);
+var pokemonList = [];
+
+pokemonList.push({
+    name: "Dragonite",
+    height: 2.2,
+    types: ["dragon", "flying"]
+});
+
+pokemonList.push({
+    name: "Scizor",
+    height: 1.8,
+    types: ["steel", "bug"]
+});
+
+pokemonList.push({
+    name: "Umbreon",
+    height: 1,
+    types: ["dark"]
+});
