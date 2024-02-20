e5f587cea12d9098613e24ab9cc128c6bd946b05
VerdeSky <140350863+VerdeSky@users.noreply.github.com>
2024-02-19 19:46:11 -0800

    added comments for clarity

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,43 @@
+// Define a module for managing a Pokemon repository
 var pokemonRepository = (function () {
+    // Private array to store the list of Pokemon
     var pokemonList = [];
 
+    // Function to retrieve all Pokemon from the repository
     function getAll() {
         return pokemonList;
     }
 
+    // Function to add a new Pokemon to the repository
     function add(pokemon) {
         pokemonList.push(pokemon);
     }
 
+    // Function to create a list item for a Pokemon and add it to the UI
     function addListItem(pokemon) {
         var listItem = document.createElement('li');
         var button = document.createElement('button');
 
+        // Set the button text to the Pokemon's name and add a CSS class
         button.innerText = pokemon.name;
         button.classList.add('pokemon-button');
 
+        // Add an event listener to show Pokemon details when the button is clicked
         button.addEventListener('click', function () {
             showDetails(pokemon);
         });
 
+        // Append the button to the list item, then the list item to the list element in the UI
         listItem.appendChild(button);
         pokemonListElement.appendChild(listItem);
     }
 
+    // Function to log the details of a Pokemon to the console
     function showDetails(pokemon) {
         console.log(pokemon);
-        // More functionality related to displaying details can be added here later
     }
 
+    // Publicly exposed functions of the module
     return {
         getAll: getAll,
         add: add,
@@ -37,7 +46,7 @@ var pokemonRepository = (function () {
     };
 })();
 
-// Adding Pokémon data
+// Adding Pokémon data to the repository
 pokemonRepository.add({
     name: "Dragonite",
     height: 2.2,
@@ -56,8 +65,10 @@ pokemonRepository.add({
     types: ["dark"]
 });
 
+// Get the list element from the DOM
 var pokemonListElement = document.querySelector('.pokemon-list');
 
+// Iterate over all Pokemon in the repository to add them to the UI
 pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
 });
